bda213f0678b21fac8634f1ed82ec6a858ed5f1b
Abelsouza777 <abel.sza@hotmail.com>
2024-10-07 01:15:28 -0300

    initials v11
    Deus é infinitamente mais!

--- a7x/src/app/page.tsx
+++ a7x/src/app/page.tsx
@@ -3,10 +3,10 @@
 export default function Home() {
   return (
     <div className="flex flex-col h-screen">
-      <div className="flex-1 bg-blue-500 text-white text-center">
+      <div className="flex-1 bg-blue-300 text-white text-center">
         ESTAMOS EM CONSTRUÇÃO
       </div>
-      <div className="flex-1 bg-green-500 text-white text-center">
+      <div className="flex-1 bg-blue-300 text-white text-center">
   <div className="m-10 border-2 border-white p-4 flex items-center justify-center">
     ENTRE EM CONTATO CONOSCO, TEREMOS PRAZER EM LHE ATENDER!
   </div>
