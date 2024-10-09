a37ca7435be92a2a4fdf27382ac4d533333844ba
Abelsouza777 <abel.sza@hotmail.com>
2024-10-09 02:11:41 -0300

    asdf
    asdfsda

--- a7x/src/app/page.tsx
+++ a7x/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-blue-900">
-      <h1 className="text-5xl font-bold mb-20">ESTAMOS EM CONSTRUÇÃO</h1>
+      <h1 className="text-xl font-bold mb-20">ESTAMOS EM CONSTRUÇÃO</h1>
       <Image
         src="/A7 LOGO.PNG" // Substitua por seu caminho da imagem
         alt="A7 STUDIO"
