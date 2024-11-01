d0dcec6a10281c382de6ea5e464c58c197aa4e61
Abelsouza777 <abel.sza@hotmail.com>
2024-10-31 22:51:31 -0300

    continuando
    continuando

--- a7x/src/app/page.tsx
+++ a7x/src/app/page.tsx
@@ -6,15 +6,26 @@ import 'react-social-icons/whatsapp'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-blue-900">
-      <h1 className="text-xl font-bold mb-20">ESTAMOS EM CONSTRUÇÃO</h1>
-      <Image
+    <div className="flex flex-col items-center justify-center min-h-screen bg-blue-100">
+      <h1 className="text-xl font-bold mb-20">SEU NEGÓCIO VENDENDO MAIS!</h1>
+      <div className="m-4 grid gap-4 sm:grid-cols-2">
+  <div className="min-h-[100px] rounded-lg bg-teal-100 shadow p-4">
+    <p>Desenvolvimento de sites</p>
+    <p>Hospedagem em nuvem</p>
+    <p>Banco de dados Postgresql</p>
+    <p>Imagens com equipamento profissional</p>
+  </div>
+  <div className="min-h-[100px] rounded-lg bg-teal-200 shadow p-4 flex flex-col items-center">
+    <p>12 ANOS DE EXPERIENCIA EM NEGÓCIOS BEM SUCEDIDOS</p>
+    <Image
         src="/A7 LOGO.PNG" // Replace with your image path
         alt="A7 STUDIO"
         width={350}
         height={350}
         className="object-cover"
       />
+  </div>
+</div>
       <p className="mt-20 text-xl">ENTRE EM CONTATO</p>
       <SocialIcon
         url="https://wa.me/45999799513"
